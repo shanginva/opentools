15a0d3daa515b7d830ead75cdc8cc4a6795d0132
Vladimir <shanginva@gmail.com>
2023-01-26 08:02:54 +0100

    Add Package for MVVM

---
+++ OpenTools.MVVM/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OpenTools.MVVM;
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public bool SetProperty<T>(ref T source, T value,
+        [CallerMemberName] string propertyName = "")
+    {
+        if (Equals(source, value))
+        {
+            return false;
+        }
+        source = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    public void OnPropertyChanged([CallerMemberName] string proerptyName = "")
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(proerptyName));
+}
